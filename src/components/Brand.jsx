400df11a9a162ff222ae4ac5032315abef840e97
= <=>
2022-07-06 04:20:37 +0100

    Changed logo to a horizontal one

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
-import BrandImg from "../images/brand.png";
+import BrandImg from "../images/logo-horizontal.png";
 
 function Brand() {
     return (

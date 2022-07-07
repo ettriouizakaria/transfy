fe68cb65fa5e68690f8d1af1c62bc4825aa61216
= <=>
2022-07-07 18:32:07 +0100

    Added a Newsletter section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import HowItWorks from '../components/home/how-it-works-section/HowItWorks'
+import Newsletter from '../components/home/newsletter-section/Newsletter'
 
 export default function Home() {
   return (
-    <HowItWorks></HowItWorks>
+    <><HowItWorks></HowItWorks><Newsletter></Newsletter></>
   )
 }

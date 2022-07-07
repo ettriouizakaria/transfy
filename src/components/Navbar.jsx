c440ebe242b41445efb713044dc3887952790cea
= <=>
2022-07-08 00:05:50 +0100

    Updated links in NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,17 @@ import Brand from "./Brand";
 import NavbarItem from "./NavbarItem";
 
 function Navbar() {
-    return (
-        <div className="navbar">
-            <Brand />
-            <div className="navbar-items">
-                <NavbarItem link="/home" title="Acceuil" />
-                <NavbarItem link="/reservation" title="Reservation" />
-                <NavbarItem link="/login" title="Connexion" />
-                <NavbarItem link="/signin" title="S'inscrire" />
-                <NavbarItem link="/contact" title="Contact" />
-            </div>
-        </div>
-    );
+  return (
+    <div className='navbar partial-width '>
+      <Brand />
+      <div className='navbar-items'>
+        <NavbarItem link='/home' title='Acceuil' />
+        <NavbarItem link='/reservation' title='Reservation' />
+        <NavbarItem link='/login' title='Connexion' />
+        <NavbarItem link='/contact' title='Contact' />
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;

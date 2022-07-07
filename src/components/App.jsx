b58b3ce62f8594fe10357ccf3f59aaf81736f878
= <=>
2022-07-08 00:09:48 +0100

    Updated routes and added more FontAwesome icons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,47 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faTrain, faMoneyCheck, faSliders } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faTrain,
+  faMoneyCheck,
+  faSliders,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebook,
+  faTwitter,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 import Home from "../pages/Home";
 import Navbar from "./Navbar";
-import Login from "../pages/Login";
 import Reservation from "../pages/reservation";
-import Signin from "../pages/Signin"
+import Login from "../pages/Login";
 
 function App() {
-  library.add(faTrain, faSliders, faMoneyCheck)
+  library.add(
+    faTrain,
+    faSliders,
+    faMoneyCheck,
+    faFacebook,
+    faTwitter,
+    faLinkedin
+  );
+
   return (
     <Router>
-      <div className="container">
+      <div className='container'>
         <Navbar />
         <Switch>
-          <Route exact path="/" >
-            <Login />
-          </Route>
-          <Route exact path="/signin" >
-            <Signin />
-          </Route>
-          <Route exact path="/home" >
+          <Route exact path='/'>
             <Home />
           </Route>
-          <Route exact path="/reservation" >
+          <Route exact path='/login'>
+            <Login />
+          </Route>
+          <Route exact path='/home'>
+            <Home />
+          </Route>
+          <Route exact path='/reservation'>
             <Reservation />
           </Route>
         </Switch>

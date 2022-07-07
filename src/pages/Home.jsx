7f79197768227d24cd25d407c5b108166ee1a06b
= <=>
2022-07-07 17:09:13 +0100

    Added the Home page and updated the styling

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import HowItWorks from '../components/home/how-it-works-section/HowItWorks'
+
+export default function Home() {
+  return (
+    <HowItWorks></HowItWorks>
+  )
+}

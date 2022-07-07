f3ba8d082b31084f86f46c36bd5aa8c2ddeb4a77
= <=>
2022-07-08 00:10:46 +0100

    Added a white TransFy logo

--- src/components/home/newsletter-section/Newsletter.jsx
+++ src/components/home/newsletter-section/Newsletter.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import PrimaryButton from '../../primary-button/PrimaryButton'
-import "./newsletter.css"
+import React from "react";
+import PrimaryButton from "../../primary-button/PrimaryButton";
+import "./newsletter.css";
 
 export default function Newsletter() {
   return (
-    <div className='section-container'>
-        <div className='section-title'> 
-            Abonnez-vous à notre newsletter
-            <p className='section-subtitle'>et recevez tout nos offres</p>
-        </div>
-        <div className="form-container">
-            <input type="text" placeholder='Entrez votre email...'></input>
-            <PrimaryButton label="Valider"></PrimaryButton>
-        </div>
+    <div className='section-container partial-width '>
+      <div className='section-title'>
+        Abonnez-vous à notre newsletter
+        <p className='section-subtitle'>et recevez tout nos offres</p>
+      </div>
+      <div className='form-container'>
+        <input type='text' placeholder='Entrez votre email...'></input>
+        <PrimaryButton label='Valider'></PrimaryButton>
+      </div>
     </div>
-  )
+  );
 }

14f24edd88406f0784da8974d3d357c8717e4274
= <=>
2022-07-09 02:12:10 +0100

    Changed NavBar component location

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,17 +3,19 @@ import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import PrimaryButton from "../components/primary-button/PrimaryButton";
 
 function Login() {
-    return (
-        <div className="form">
-            <form >
-                <h1>Se connecter</h1>
-                <input type="text" placeholder="E-mail" />
-                <input type="password" placeholder="Mot de passe" />
-                <PrimaryButton label="S'authentifier" marginTop="15"></PrimaryButton>
-            </form>
-            <p>Vous n'avez pas un compte? <Link to="/signin">S'inscrire</Link> </p>
-        </div>
-    );
+  return (
+    <div className='form'>
+      <form>
+        <h1>Se connecter</h1>
+        <input type='text' placeholder='E-mail' />
+        <input type='password' placeholder='Mot de passe' />
+        <PrimaryButton label="S'authentifier" marginTop='15'></PrimaryButton>
+      </form>
+      <p>
+        Vous n'avez pas un compte? <Link to='/signup'>S'inscrire</Link>
+      </p>
+    </div>
+  );
 }
 
 export default Login;

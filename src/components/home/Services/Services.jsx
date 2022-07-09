4ac57e21112cc685f7493fa9f19eb82bc8e2a47b
ettriouizakaria <zakaria.ettrioui@usmba.ac.ma>
2022-07-09 03:21:09 +0100

    add home components & its styles

--- src/components/home/Services/Services.jsx
+++ src/components/home/Services/Services.jsx
@@ -8,7 +8,7 @@ export default function Services() {
             <h1>Nos services</h1>
             <Service  icon="home" title="Réservez sans vous deplacer" content="Tout ce dont vous avez besoin est votre appareil, réservez depuis chez vous et gagnez du temps!" />
 
-            <Service s icon="turn_right" title="Avoir l’itinéraire complet" content="TransFy vous fournit l'itinéraire complet de votre voyage, du point de départ jusqu'au point d'arrivée. " />
+            <Service  icon="turn_right" title="Avoir l’itinéraire complet" content="TransFy vous fournit l'itinéraire complet de votre voyage, du point de départ jusqu'au point d'arrivée. " />
 
             <Service  icon="tune" title="Filtrer vos choix" content="Le système de filtrage du TransFy vous permez de filtrer les itinéraires en fonction de vos critères " />
 

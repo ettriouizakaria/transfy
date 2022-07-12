e728f485a818c41d00b3398a941e04affb1bf65b
AMN009 <aymennorhalloussi@gmail.com>
2022-07-12 19:13:03 +0100

    map added successfully

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Footer from "../components/home/footer/Footer";
 import HowItWorks from "../components/home/how-it-works-section/HowItWorks";
 import Newsletter from "../components/home/newsletter-section/Newsletter";
-import Services from "../components/home/services/Services";
+import Services from "../components/home/Services/Services";
 import SplashScreen from "../components/home/splash-screen/SplashScreen";
 
 export default function Home() {
--- src/pages/reservation.jsx
+++ src/pages/reservation.jsx
@@ -1,111 +1,116 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import PrimaryButton from "../components/primary-button/PrimaryButton";
 import Slider from "@material-ui/core/Slider";
-import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
 
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW1uMDA5IiwiYSI6ImNrZ3FrN25nYjA1MXQycW1mc3Y2cnV4MGYifQ.HH70YXWvUre4p-ZTxjzV5A';
 
 function Reservation() {
     const [val2, setVal2] = React.useState([1, 5]);
-    const Map = ReactMapboxGl({
-        accessToken:
-            'pk.eyJ1IjoiZmFicmljOCIsImEiOiJjaWc5aTV1ZzUwMDJwdzJrb2w0dXRmc2d0In0.p6GGlfyV-WksaDV_KdN27A'
+
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-7.603869);
+    const [lat, setLat] = useState(33.589886);
+    const [zoom, setZoom] = useState(5);
+
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [lng, lat],
+            zoom: zoom
+        });
     });
+
     return (
-        <div className="inline-divs">
+        <>
+            <div className="inline-divs">
 
-            <div className="form">
-                <form >
-                    <h1>Reserver votre ticket</h1>
-                    <h4>Traget  </h4>
-                    <div className="input">
-                        <input type="text" placeholder=" Point de Départ" />
-                    </div>
-
-                    <div className="input">
-                        <input type="text" placeholder="Point de Destination" />
-                    </div>
-
-                    <h4>Date  </h4>
-
-                    <div className="input">
-
-                        <input type="Date" placeholder="Point de Destination" />
-
-                    </div>
-
-                    <h4>Critères  </h4>
-
-                    <div className="input">
-                        <h3>Confort : </h3>
-                        <Slider
-                            step={1}
-                            min={1}
-                            max={5}
-                            value={val2}
-                            onChange={(ev, v) => setVal2(v)}
-                            onChangeCommitted={(ev, v) => console.log(v)}
-                            valueLabelDisplay="off"
-                            aria-labelledby="range-slider"
-                        />
-                        <div className="criteria-range">
-                            <div>{`${val2[0]} (moins confortable)`}</div>
-                            <div>{`${val2[1]} (Plus confortable)`}</div>
+                <div className="form">
+                    <form >
+                        <h1>Reserver votre ticket</h1>
+                        <h4>Traget  </h4>
+                        <div className="input">
+                            <input type="text" placeholder=" Point de Départ" />
                         </div>
-                    </div>
-                    <div className="input">
-                        <h3>Prix : </h3>
-                        <Slider
-                            step={1}
-                            min={1}
-                            max={5}
-                            value={val2}
-                            onChange={(ev, v) => setVal2(v)}
-                            onChangeCommitted={(ev, v) => console.log(v)}
-                            valueLabelDisplay="off"
-                            aria-labelledby="range-slider"
-                        />
-                        <div className="criteria-range">
-                            <div>{`${val2[0]} (Moins chère)`}</div>
-                            <div>{`${val2[1]} (Plus chère)`}</div>
+
+                        <div className="input">
+                            <input type="text" placeholder="Point de Destination" />
                         </div>
-                    </div>
-                    <div className="input">
-                        <h3>Duréé: </h3>
-                        <Slider
-                            step={1}
-                            min={1}
-                            max={5}
-                            value={val2}
-                            onChange={(ev, v) => setVal2(v)}
-                            onChangeCommitted={(ev, v) => console.log(v)}
-                            valueLabelDisplay="off"
-                            aria-labelledby="range-slider"
-                        />
-                        <div className="criteria-range">
-                            <div>{`${val2[0]} (Durée longue)`}</div>
-                            <div>{`${val2[1]} (Durée courte)`}</div>
+
+                        <h4>Date  </h4>
+
+                        <div className="input">
+
+                            <input type="Date" placeholder="Point de Destination" />
+
                         </div>
-                    </div>
+
+                        <h4>Critères  </h4>
+
+                        <div className="input">
+                            <h3>Confort : </h3>
+                            <Slider
+                                step={1}
+                                min={1}
+                                max={5}
+                                value={val2}
+                                onChange={(ev, v) => setVal2(v)}
+                                onChangeCommitted={(ev, v) => console.log(v)}
+                                valueLabelDisplay="off"
+                                aria-labelledby="range-slider"
+                            />
+                            <div className="criteria-range">
+                                <div>{`${val2[0]} (moins confortable)`}</div>
+                                <div>{`${val2[1]} (Plus confortable)`}</div>
+                            </div>
+                        </div>
+                        <div className="input">
+                            <h3>Prix : </h3>
+                            <Slider
+                                step={1}
+                                min={1}
+                                max={5}
+                                value={val2}
+                                onChange={(ev, v) => setVal2(v)}
+                                onChangeCommitted={(ev, v) => console.log(v)}
+                                valueLabelDisplay="off"
+                                aria-labelledby="range-slider"
+                            />
+                            <div className="criteria-range">
+                                <div>{`${val2[0]} (Moins chère)`}</div>
+                                <div>{`${val2[1]} (Plus chère)`}</div>
+                            </div>
+                        </div>
+                        <div className="input">
+                            <h3>Duréé: </h3>
+                            <Slider
+                                step={1}
+                                min={1}
+                                max={5}
+                                value={val2}
+                                onChange={(ev, v) => setVal2(v)}
+                                onChangeCommitted={(ev, v) => console.log(v)}
+                                valueLabelDisplay="off"
+                                aria-labelledby="range-slider"
+                            />
+                            <div className="criteria-range">
+                                <div>{`${val2[0]} (Durée longue)`}</div>
+                                <div>{`${val2[1]} (Durée courte)`}</div>
+                            </div>
+                        </div>
 
 
 
-                    <PrimaryButton label="Chercher" marginTop="15"></PrimaryButton>
-                </form>
+                        <PrimaryButton label="Chercher" marginTop="15"></PrimaryButton>
+                    </form>
+                </div>
+
             </div>
-            <div className="map">
-                <Map
-                    style={{mapbox:"//styles/mapbox/streets-v9"}}
-                    containerStyle={{
-                        height: '400px',
-                        width: '500px'
-                    }}
-                >
-                    <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
-                        <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-                    </Layer>
-                </Map>
-            </div>
-        </div>
+            <div ref={mapContainer} className="map-container" />
+        </>
     );
 }
 

1c8f66cba279b4703832a04ec8aa6b6cdb51b32f
AMN009 <aymennorhalloussi@gmail.com>
2022-07-12 20:05:46 +0100

    mapbox part done

--- src/pages/reservation.jsx
+++ src/pages/reservation.jsx
@@ -8,11 +8,109 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYW1uMDA5IiwiYSI6ImNrZ3FrN25nYjA1MXQycW1mc3Y2c
 function Reservation() {
     const [val2, setVal2] = React.useState([1, 5]);
 
+    const [items, setItems] = useState([]);
+
     const mapContainer = useRef(null);
     const map = useRef(null);
     const [lng, setLng] = useState(-7.603869);
     const [lat, setLat] = useState(33.589886);
     const [zoom, setZoom] = useState(5);
+
+    const handleChange = (event) => {
+        let val = event.target.value.toString()
+        fetch("https://api.mapbox.com/geocoding/v5/mapbox.places/" + val + ".json?language=fr&country=ma,eh&autocomplete=true&types=place&access_token=" + mapboxgl.accessToken)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    let s = []
+                    result.features.forEach(e => {
+                        let o = {}
+                        o.id = e.id
+                        o.name = e.text_fr
+                        s.push(o)
+                    });
+                    setItems(s);
+                },
+                (error) => {}
+            )
+    }
+
+    async function getRoute(start, end) {
+        // make a directions request using cycling profile
+        // an arbitrary start will always be the same
+        // only the end or destination will change
+        const query = await fetch(
+            `https://api.mapbox.com/directions/v5/mapbox/driving/${start.lon},${start.lat};${end.lon},${end.lat}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+            { method: 'GET' }
+        );
+        const json = await query.json();
+        const data = json.routes[0];
+        const route = data.geometry.coordinates;
+        const geojson = {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'LineString',
+                coordinates: route
+            }
+        };
+        // if the route already exists on the map, we'll reset it using setData
+        if (map.current.getSource('route')) {
+            map.current.getSource('route').setData(geojson);
+        }
+        // otherwise, we'll make a new request
+        else {
+            map.current.addLayer({
+                id: 'route',
+                type: 'line',
+                source: {
+                    type: 'geojson',
+                    data: geojson
+                },
+                layout: {
+                    'line-join': 'round',
+                    'line-cap': 'round'
+                },
+                paint: {
+                    'line-color': '#1363DF',
+                    'line-width': 5,
+                    'line-opacity': 0.75
+                }
+            });
+        }
+    }
+
+    const handleClick = (event) => {
+        let startCityCoor = {}, destinationCityCoor = {}
+        const startCityInput = document.getElementById("start-city").value
+        const destinationCityInput = document.getElementById("destination-city").value
+        fetch("https://api.mapbox.com/geocoding/v5/mapbox.places/" + startCityInput + ".json?language=fr&country=ma,eh&autocomplete=true&types=place&limit=1&access_token=" + mapboxgl.accessToken)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    let lon = result.features[0].center[0]
+                    let lat = result.features[0].center[1]
+                    startCityCoor.lon = lon
+                    startCityCoor.lat = lat
+                    fetch("https://api.mapbox.com/geocoding/v5/mapbox.places/" + destinationCityInput + ".json?language=fr&country=ma,eh&autocomplete=true&types=place&limit=1&access_token=" + mapboxgl.accessToken)
+                        .then(res => res.json())
+                        .then(
+                            (result) => {
+                                result.features.forEach(e => {
+                                    let lon = result.features[0].center[0]
+                                    let lat = result.features[0].center[1]
+                                    destinationCityCoor.lon = lon
+                                    destinationCityCoor.lat = lat
+                                    getRoute(startCityCoor, destinationCityCoor)
+                                });
+                            },
+                            (error) => { }
+                        )
+                },
+                (error) => { }
+            )
+
+    }
 
     useEffect(() => {
         if (map.current) return; // initialize map only once
@@ -29,15 +127,24 @@ function Reservation() {
             <div className="inline-divs">
 
                 <div className="form">
-                    <form >
                         <h1>Reserver votre ticket</h1>
                         <h4>Traget  </h4>
                         <div className="input">
-                            <input type="text" placeholder=" Point de Départ" />
+                            <input id="start-city" onChange={handleChange} list="start-cities" type="text" placeholder=" Point de Départ" />
+                            <datalist id="start-cities">
+                                {items.map(item => (
+                                    <option key={"start-" + item.id} value={item.name} />
+                                ))}
+                            </datalist>
                         </div>
 
                         <div className="input">
-                            <input type="text" placeholder="Point de Destination" />
+                            <input id="destination-city" onChange={handleChange} list="destination-cities" type="text" placeholder="Point de Destination" />
+                            <datalist id="destination-cities">
+                                {items.map(item => (
+                                    <option key={"destination-" + item.id} value={item.name} />
+                                ))}
+                            </datalist>
                         </div>
 
                         <h4>Date  </h4>
@@ -85,7 +192,7 @@ function Reservation() {
                             </div>
                         </div>
                         <div className="input">
-                            <h3>Duréé: </h3>
+                            <h3>Durée: </h3>
                             <Slider
                                 step={1}
                                 min={1}
@@ -104,8 +211,7 @@ function Reservation() {
 
 
 
-                        <PrimaryButton label="Chercher" marginTop="15"></PrimaryButton>
-                    </form>
+                        <PrimaryButton onClick={handleClick} label="Chercher" marginTop="15"></PrimaryButton>
                 </div>
 
             </div>

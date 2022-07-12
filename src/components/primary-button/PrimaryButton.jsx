d4c68bd9300b3ac89a133dbe85cce1056636bd4a
= <=>
2022-07-12 02:00:26 +0100

    Updated global styling and added onClick to button

--- src/components/primary-button/PrimaryButton.jsx
+++ src/components/primary-button/PrimaryButton.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import styles from "./PrimaryButton.module.css"
+import React from "react";
+import styles from "./PrimaryButton.module.css";
 
 export default function PrimaryButton(props) {
   let inlineStyle = {
-    marginTop: props.marginTop + "px"
-  }
+    marginTop: props.marginTop + "px",
+  };
   return (
-    <button style={inlineStyle} className={styles.PrimaryButton} >{props.label}</button>
-  )
+    <button
+      style={inlineStyle}
+      className={styles.PrimaryButton}
+      onClick={props.onClick}>
+      {props.label}
+    </button>
+  );
 }

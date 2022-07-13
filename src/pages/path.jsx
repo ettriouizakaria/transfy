0bdb402f2cf95404d64b395140479e37101e3af3
= <th3salman@gmail.com>
2022-07-13 22:15:08 +0100

    Updated the reservation page styling

--- src/pages/path.jsx
+++ src/pages/path.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
-import Rating from '@mui/material/Rating';
+import React from "react";
+import Rating from "@mui/material/Rating";
 
 export default function Path(props) {
- 
   return (
-    
     <div className='path'>
-        <h3>{props.type}</h3>
-        <div className='path-details'>{props.from} --> {props.to}</div>
-        <span className='path-details'> Evaluation</span>  <Rating name="read-only" value={props.rating} readOnly />
-        <div className='path-price'>Cout approximative : {props.price} DH</div>
+      <h3>{props.type}</h3>
+
+      <div className='path-details-container'>
+        <div className='path-detail-label'> Trajet:</div>
+        <div className='path-detail-value'>
+          {props.from} {"-->"} {props.to}
+        </div>
+      </div>
+
+      {props.rating && (
+        <div className='path-details-container'>
+          <span className='path-detail-label'> Evaluation:</span>
+          <Rating
+            className='path-detail-value'
+            name='read-only'
+            value={props.rating}
+            readOnly
+          />
+        </div>
+      )}
+
+      <div className='path-details-container'>
+        <div className='path-detail-label'>
+          {props.price.approximate ? "Côut approximatif :" : "Côut"}
+        </div>
+        <div className='path-detail-value'> {props.price.value} DH</div>
+      </div>
     </div>
-  )
+  );
 }
